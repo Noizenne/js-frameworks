65d54625f212ae5e18f6813d0f85dd64d3db48fd
Noizenne <karlamae2405@gmail.com>
2023-03-20 19:46:15 +0100

    Created Search function

---
+++ src/components/Search/index.jsx
@@ -0,0 +1,34 @@
+import React from "react";
+import { Search } from "@mui/icons-material";
+import * as S from "../styles/Search.styles";
+/**
+ * This will perform a search input
+ */
+export default function SearchBar({searchInput, onSearchInputChange, filteredProducts}) {
+
+    function onChange(event) {
+        onSearchInputChange(event.currentTarget.value)
+    }
+
+    return (
+        <S.StyledSearch>
+            <S.StyledInputWrapper>
+                <Search 
+                fontSize="large" 
+                sx={{color: "white"}} 
+                />
+                <S.StyledInput 
+                onChange={onChange} 
+                value={searchInput} 
+                placeholder="Search..."/>
+            </S.StyledInputWrapper>
+            <S.StyledSearchResults> 
+                {filteredProducts.map((item) => (
+                    <S.StyledSearchedItem href={`/src/pages/product/${item.id}`} key={item.id}>
+                        {item.title}
+                    </S.StyledSearchedItem>
+                ))}
+            </S.StyledSearchResults>
+        </S.StyledSearch>
+    )
+}
--- src/components/styles/Search.styles.jsx
+++ src/components/styles/Search.styles.jsx
@@ -1,5 +1,56 @@
 import styled from "styled-components"
 
 export const StyledSearch = styled.div`
+    display: flex;
+    flex-direction: column;
+    margin-left: auto;
+    margin-right: auto;
+    width: 300px;
     
+    
+`
+export const StyledInputWrapper = styled.div`
+    display: flex;
+    width: 100%;
+    background-color: ${({theme}) => theme.colors.nav};
+    border-radius: 5px;
+    padding: 3px;
+`
+
+export const StyledInput = styled.input`
+    border: none;
+    width: 100%;
+    border-radius: 10px;
+    background-color: transparent;
+    color: ${({theme}) => theme.colors.color};
+    margin-left: 5px;
+
+    :focus {
+        outline: none;
+    }
+
+    ::placeholder {
+        color: ${({theme}) => theme.colors.color};
+    }
+`
+
+export const StyledSearchResults = styled.div`
+    width: 100%;
+    border: 0.5px solid ${({theme}) => theme.colors.nav};
+    border-top: none;
+    box-shadow: rgba(149, 157, 165, 0.2) 0px 8px 24px;
+`
+
+export const StyledSearchedItem = styled.a`
+    display: flex;
+    justify-content: left;
+    height: 30px;
+    margin-left: 10px;
+    margin-top: 10px;
+    text-decoration: none;
+    color: black;
+
+    :hover {
+        font-size: 16.5px;
+}
 `
