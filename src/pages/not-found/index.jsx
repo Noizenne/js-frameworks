c01788b279edc07384dd932f7612341a67c900f6
Karla <73695367+Noizenne@users.noreply.github.com>
2023-04-01 20:42:54 +0200

    Added NotFound page

---
+++ src/pages/not-found/index.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+import { StyledNotFound } from "../../components/styles/NotFound.styles";
+import { Block } from "@mui/icons-material";
+
+export default function NotFound() {
+    return (
+        <StyledNotFound>
+            <div><Block fontSize="large" style={{color: 'red'}}/></div>
+            <h1>PAGE NOT FOUND</h1>
+            <p>The link you have followed may be broken or the page may have been removed.</p>
+        </StyledNotFound>
+    )
+}
