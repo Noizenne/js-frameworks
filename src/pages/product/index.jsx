f9ee4ae5fe6c9f32372a432728754277e5515edd
Noizenne <karlamae2405@gmail.com>
2023-04-02 11:49:00 +0200

    Document title eslint

--- src/pages/product/index.jsx
+++ src/pages/product/index.jsx
@@ -29,8 +29,8 @@ export default function Product() {
         }
       }
 
-      document.title = "Dings | " + product.title;
-      
+      document.title = `Dings | ${product.title}`;
+
       getProduct(API_URL + `/${id}`);
   
     }, [id]);
